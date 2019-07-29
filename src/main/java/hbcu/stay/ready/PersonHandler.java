3951be8241c3239388b7619b8b3e3169bfa5af1e
adolison <amdol1@morgan.edu>
2019-07-28 20:20:12 -0400

    All loops done

--- src/main/java/hbcu/stay/ready/PersonHandler.java
+++ src/main/java/hbcu/stay/ready/PersonHandler.java
@@ -11,14 +11,20 @@ public class PersonHandler {
     public String whileLoop() {
         String result = "";
         // create a `counter`
+        int a = 0;
         // while `counter` is less than length of array
+        do {result = result+personArray[a];
+        a++;}
+        while (a<personArray.length);
+
             // begin loop
 
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
+            // use `counter` to identify the `current Person` in the array
+            // get `string Representation` of `currentPerson`
+            // append `stringRepresentation` to `result` variable
 
             // end loop
+
         return result;
     }
 
@@ -29,7 +35,9 @@ public class PersonHandler {
         // identify initial value
         // identify terminal condition
         // identify increment
-
+            for (int b=0;b<personArray.length;b++){
+               result = result+personArray[b];
+            }
         // use the above clauses to declare for-loop signature
             // begin loop
                 // use `counter` to identify the `current Person` in the array
@@ -53,11 +61,19 @@ public class PersonHandler {
                 // append `stringRepresentation` to `result` variable
             // end loop
 
+        for(Person c : personArray){
+            result = result+c;
+
+        }
+
         return result;
     }
 
 
     public Person[] getPersonArray() {
+        for(Person c : personArray){
+
+        }
         return personArray;
     }
 }
